f0d9f75926006cda8428a4d0f3ba4d165d55c2ab
Seif El-Din Sweilam <saifsweelam@gmail.com>
2020-03-20 23:20:32 +0200

    Modify Risponsiveness & test typewriter

---
+++ script/type.js
@@ -0,0 +1,23 @@
+const text = document.querySelector('.type')
+
+const content = text.textContent.replace(/[\n\r]+|[\s]{2,}/g, ' ').trim()
+let counter = 0
+text.textContent = ''
+
+/*
+while (counter < content.length) {
+    setTimeout((function (counterCopy) {
+        return function () {
+            text.textContent += content[counterCopy];
+        }
+    }(counter)), 100 * counter++)
+}
+*/
+
+var typewriter = setInterval(function(){
+    text.textContent += content[counter];
+    counter++;
+    if (counter > (content.length-1)) {
+        clearInterval(typewriter);
+    }
+}, 100);
